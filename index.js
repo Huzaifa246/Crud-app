94ba003f9c9a5f7d5e6d5198d947997a07a6a099
Huzaifa Rehman <60300174+Huzaifa246@users.noreply.github.com>
2022-06-18 11:01:19 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,87 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mysql = require('mysql'); 
+
+app.use(express.json());
+
+app.use(cors());
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//database connection
+
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: 'root',
+    database: 'huzaifa',
+});
+
+//post request
+
+app.post('/create', (req, res) => {
+    const fname = req.body.fname;
+    const lname = req.body.lname;
+    const fathname = req.body.fathname;
+    const cnic = req.body.cnic;
+    const gender = req.body.gender;
+    
+    db.query('INSERT INTO details(fname, lname, fathname,cnic,gender) VALUES (?,?,?,?,?)', [fname, lname, fathname, cnic, gender], (err, result) => {
+        if(err){
+            console.log(err)
+        }else{
+            res.send("success");
+        }
+    });
+
+});
+
+// update
+app.put("/update",(req,res)=>{
+    const id = req.body.id;
+    const fname = req.body.fname;
+    const lname = req.body.lname;
+    const fathname = req.body.fathname;
+    const cnic = req.body.cnic;
+    const gender = req.body.gender;
+
+    db.query("UPDATE details SET fname = ?, lname = ?, fathname = ?, cnic = ?, gender = ? WHERE id= ?",
+    [fname,lname,fathname,cnic,gender,id],
+    (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(result);
+        }
+      });
+});
+
+app.delete("/delete/:id", (req, res) =>{
+    const id = req.params.id;
+    db.query("DELETE FROM details WHERE id = ?", id, (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(result);
+        }
+      });
+    });
+
+//get request
+
+app.get('/get', (req, res) => {
+    db.query('SELECT * FROM details', (err,result) => {
+
+        if(err){
+            console.log(err);
+        }else{
+            res.send(result);
+        }
+    });
+});
+
+
+app.listen(5000, () => {
+
+    console.log("server is running on port 5000");
+})
